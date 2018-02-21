3deffde86d1deed2e14a29b10b4b3e82665346b5
Charles Mattei <sygmajji@gmail.com>
2018-02-20 19:55:56 -0800

    Modify server error handler.

--- src/server/dev-server.js
+++ src/server/dev-server.js
@@ -53,21 +53,16 @@ app.use(function(req, res, next) {
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found')
-  let status = 404;
-  next({err, status})
+  let err = new Error('404: Not Found')
+  next(err)
 })
 
 // Error handler
+// TODO See https://expressjs.com/en/guide/error-handling.html
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.send("Error " + (err.status || 500))
-});
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 // Netowork
 io.on('connection', function(socket) {
